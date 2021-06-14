5526872d2d92445e8aa497d7ed668c9ae835538a
Dracon <petermarkcarey@gmail.com>
2021-06-14 21:31:42 +0800

    asdfg

--- BehaviourTree/Character.cs
+++ BehaviourTree/Character.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu()]
+[CreateAssetMenu(fileName = "character", menuName = "Data/Character")]
 public class Character : ScriptableObject
 {
     public string Name;
