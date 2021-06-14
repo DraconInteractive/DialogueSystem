3033bd4fa9e6ccbe56a0676e95a5ec23dc05d204
Dracon <petermarkcarey@gmail.com>
2021-06-14 19:03:40 +0800

    init

---
+++ BehaviourTree/DialogueNode.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueNode : CompositeNode
+{
+    [TextArea()]
+    public string Text;
+    [Space()]
+    public string Character;
+    public float duration;
+    public string Sequence;
+
+    protected override void OnStart()
+    {
+    }
+
+    protected override void OnStop()
+    {
+    }
+
+    protected override State OnUpdate()
+    {
+        return State.Success;
+    }
+}
