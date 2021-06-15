953f970bb77cdd924776e2e89e227191b661476b
Dracon <petermarkcarey@gmail.com>
2021-06-15 13:06:03 +0800

    Isolate character

--- BehaviourTree/Character.cs
+++ BehaviourTree/Character.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "character", menuName = "Data/Character")]
-public class Character : ScriptableObject
+namespace DI_Dialogue
 {
-    public string Name;
-    public Sprite Icon;
+    [CreateAssetMenu(fileName = "character", menuName = "Data/Character")]
+    public class Character : ScriptableObject
+    {
+        public string Name;
+        public Sprite Icon;
+    }
 }
+
